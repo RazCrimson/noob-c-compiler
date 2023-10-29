313d3315565258c35f4f580fed321f5840f238fc
Bharath Vignesh J K <52282402+RazCrimson@users.noreply.github.com>
2023-10-30 04:48:35 +0530

    add: comments support

--- example.c
+++ example.c
@@ -1,16 +1,31 @@
+/*
+
+Sample program to show off an example for noob C compiler
+
+*/
+
+
+// Referencing an external Cpp module function
 extern void printInt(int val);
 
+// Simple Function with a return value
 int doMath(int a) {
   int x = a * 5;
+
+  // Conditional Statement
   if(x < 20) {
     printInt(100 + x);
   } else {
     printInt(x);
   }
+  
   return x + 3;
 }
 
+
+
 int main() {
+  /* Driver function for the program */
   int a = 10;
   echo(a * 20 + 2);
   echo(a < 20);
@@ -20,6 +35,7 @@ int main() {
   printInt(doMath(a));
   printInt(doMath(4));
 
+  // Loop statement
   int b = 0;
   while(b < 10) {
     printInt(b);
